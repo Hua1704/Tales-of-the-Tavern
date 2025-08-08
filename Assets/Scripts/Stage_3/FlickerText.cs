64b5182f80957a4d2e9914dfca3280e2085d3fa4
Nhật Thanh <nhatthanhtran2606@gmail.com>
2025-08-08 11:18:25 +0700

    Add Tutorial for map 1

--- Assets/Scripts/Stage_3/FlickerText.cs
+++ Assets/Scripts/Stage_3/FlickerText.cs
@@ -3,7 +3,7 @@ using TMPro;
 
 public class FlickerText : MonoBehaviour
 {
-    public GameObject tutorialCanvas;  // Assign your TutorialCanvas here in Inspector
+    public GameObject tutorialCanvas;
     public float flickerSpeed = 1f;    // Flickers per second
 
     private TextMeshProUGUI tmpText;
@@ -22,15 +22,14 @@ public class FlickerText : MonoBehaviour
             if (!tmpText.gameObject.activeSelf)
                 tmpText.gameObject.SetActive(true);
 
-            // Flicker alpha between 0 and 1
-            float alpha = (Mathf.Sin(Time.time * flickerSpeed * Mathf.PI * 2) + 1f) / 2f;
+            // Use unscaledTime so flicker continues when timeScale = 0
+            float alpha = (Mathf.Sin(Time.unscaledTime * flickerSpeed * Mathf.PI * 2) + 1f) / 2f;
             Color c = tmpText.color;
             c.a = alpha;
             tmpText.color = c;
         }
         else
         {
-            // Hide the text completely when canvas is off
             if (tmpText.gameObject.activeSelf)
                 tmpText.gameObject.SetActive(false);
         }
